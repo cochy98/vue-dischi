1f20d6c0525df758c00c20aaec851bdc9a5c4a25
cochy98 <cosmoferrigno@gmail.com>
2022-03-30 17:44:52 +0200

    fix css single card

--- src/components/DiscCard.vue
+++ src/components/DiscCard.vue
@@ -1,10 +1,12 @@
 <template>
   <!-- Single card -->
   <div class="col">
-    <div class="card h-100">
-      <img :src="discInfo.poster" class="card-img-top" :alt="discInfo.title" />
-      <div class="card-body text-center">
+    <div class="card h-100 text-center">
+      <img class="card-img-top" :src="discInfo.poster" :alt="discInfo.title" />
+      <div class="card-body">
         <h3 class="card-title">{{ discInfo.title }}</h3>
+      </div>
+      <div class="card-footer">
         <p class="card-text m-0">{{ discInfo.author }}</p>
         <p class="card-text">{{ discInfo.year }}</p>
       </div>
@@ -22,5 +24,33 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+/* Importo _variables.scss */
+@import "../assets/scss/partials/_variables.scss";
+
+div.card {
+  background-color: $primary-color;
+  border: none;
+  border-radius: 0;
+
+  img.card-img-top {
+    padding: 1.1rem;
+  }
+
+  h3.card-title {
+    text-transform: uppercase;
+    color: white;
+  }
+
+  .card-footer {
+    padding: 0.5rem 1rem;
+    background-color: transparent;
+    border-top: none;
+    padding: 2rem;
+
+    p.card-text {
+      color: $description-color;
+    }
+  }
+}
 </style>
